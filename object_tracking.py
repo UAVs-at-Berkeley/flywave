fd3362efc0499dbda3ee7ba512dc0e19a6f86a63
Alex Chan <alex.chan@berkeley.edu>
2018-04-10 17:36:18 -0700

    raw video feed stream

--- object_tracking.py
+++ object_tracking.py
@@ -17,9 +17,8 @@ class UserVision:
     def save_pictures(self, args):
         #print("saving picture")
         img = self.vision.get_latest_valid_picture()
-        cv2.imshow(img)
-        filename = "test_image_%06d.png" % self.index
-        #cv2.imwrite(filename, img)
+        filename = "/images/test_image_%06d.png" % self.index
+        cv2.imwrite(filename, img)
         self.index +=1
 
     def detect(self, args):
@@ -47,14 +46,18 @@ if (success):
         # skipping actually flying for safety purposes indoors - if you want
         # different pictures, move the bebop around by hand
         print("Fly me around by hand!")
-        bebop.smart_sleep(5)
+        # bebop.smart_sleep(5)
         print("Moving the camera using velocity")
         bebop.pan_tilt_camera_velocity(pan_velocity=0, tilt_velocity=-2, duration=4)
         #
         # bebop.safe_takeoff(10)
+        while True:
+            bebop.smart_sleep(1)
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
 
 
-        bebop.smart_sleep(30)
+        # bebop.smart_sleep(50)
         # bebop.safe_land(10)
         print("Finishing demo and stopping vision")
         bebopVision.close_video()
