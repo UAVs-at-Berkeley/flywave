a74b722bd871d993a07ba3325b329dc8bf6ae9c1
Alex Chan <alex.chan@berkeley.edu>
2018-04-23 01:03:42 -0700

    Added keras models

---
+++ cnn_model.py
@@ -0,0 +1,38 @@
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+import cv2
+import io
+import os
+from PIL import Image
+
+import keras
+from keras.datasets import mnist
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Flatten
+from keras.layers import Conv2D, MaxPooling2D
+from keras import backend as K
+
+def createModel():
+    model = Sequential()
+    model.add(Conv2D(32, (3, 3), padding='same', activation='relu', input_shape=input_shape))
+    model.add(Conv2D(32, (3, 3), activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(0.25))
+
+    model.add(Conv2D(64, (3, 3), padding='same', activation='relu'))
+    model.add(Conv2D(64, (3, 3), activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(0.25))
+
+    model.add(Conv2D(64, (3, 3), padding='same', activation='relu'))
+    model.add(Conv2D(64, (3, 3), activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(0.25))
+
+    model.add(Flatten())
+    model.add(Dense(512, activation='relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(nClasses, activation='softmax'))
+
+    return model
---
+++ train.py
@@ -0,0 +1,105 @@
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+import cv2
+import io
+import os
+from PIL import Image
+
+import keras
+from keras.datasets import mnist
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Flatten
+from keras.layers import Conv2D, MaxPooling2D
+from keras import backend as K
+from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
+
+def preprocess(img):
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    blur = cv2.GaussianBlur(img,(5,5),0)
+    ret, img = cv2.threshold(blur,70,255,cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)
+    img = cv2.resize(img, (img.shape[0]//2, img.shape[1]//2))
+    return img
+
+def rgb2gray(rgb):
+    return np.dot(rgb[...,:3], [0.299, 0.587, 0.114])
+def show_image(a,save=False,save_fname=None):
+    ''' display the image using matplotlib'''
+    plt.figure(figsize=(8,6))
+    a = np.uint8(np.clip(a, 0, 255))
+    plt.imshow(a)
+    plt.show()
+    if save:
+        if save_fname is None:
+            raise ValueError('save_fname must be set if save=True')
+        plt.imsave(save_fname,a)
+
+
+
+data = []
+labels = []
+label_index = 0
+
+for label in os.listdir(DATAPATH):
+    if label == 'ok' or label == 'rock':
+        img_dir = DATAPATH + label + '/'
+        for fn in os.listdir(img_dir):
+            img = cv2.imread(img_dir + fn, 1)
+            img = preprocess(img)
+            data.append(img)
+            labels.append(label_index)
+        label_index += 1
+
+print("Finished loading data")
+
+data = np.array(data)
+labels = np.array(labels)
+
+np.save('data.npy', data)
+np.save('labels.npy', labels)
+
+x_train, x_test, y_train, y_test = train_test_split(data, labels, test_size=0.2, random_state=6)
+
+batch_size = 128
+num_classes = 2
+epochs = 12
+channels = 1
+# input image dimensions
+img_rows, img_cols = 360, 640
+
+# the data, split between train and test sets
+
+if K.image_data_format() == 'channels_first':
+    x_train = x_train.reshape(x_train.shape[0], channels, img_rows, img_cols)
+    x_test = x_test.reshape(x_test.shape[0], channels, img_rows, img_cols)
+    input_shape = (channels, img_rows, img_cols)
+else:
+    x_train = x_train.reshape(x_train.shape[0], img_rows, img_cols, channels)
+    x_test = x_test.reshape(x_test.shape[0], img_rows, img_cols, channels)
+    input_shape = (img_rows, img_cols, channels)
+
+x_train = x_train.astype('float32')
+x_test = x_test.astype('float32')
+x_train /= 255
+x_test /= 255
+print('x_train shape:', x_train.shape)
+print(x_train.shape[0], 'train samples')
+print(x_test.shape[0], 'test samples')
+
+# convert class vectors to binary class matrices
+y_train = keras.utils.to_categorical(y_train, num_classes)
+y_test = keras.utils.to_categorical(y_test, num_classes)
+
+model.compile(loss=keras.losses.categorical_crossentropy,
+              optimizer=keras.optimizers.Adadelta(),
+              metrics=['accuracy'])
+
+model.fit(x_train, y_train,
+          batch_size=batch_size,
+          epochs=epochs,
+          verbose=1,
+          validation_data=(x_test, y_test))
+score = model.evaluate(x_test, y_test, verbose=0)
+print('Test loss:', score[0])
+print('Test accuracy:', score[1])
