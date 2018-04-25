986466f0481b44399b9fe9d54fe16ef9a0a8aa2d
Alex Chan <alex.chan@berkeley.edu>
2018-04-24 17:24:59 -0700

    Threshold code

---
+++ preprocess_test.py
@@ -0,0 +1,34 @@
+import numpy as np
+import pandas as pd
+import cv2
+import io
+import os
+from PIL import Image
+
+def preprocess(img, scale):
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    blur = cv2.GaussianBlur(img,(5,5),0)
+    ret, img = cv2.threshold(blur,70,255,cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)
+    img = cv2.resize(img, (img.shape[1]//scale, img.shape[0]//scale))
+    return img
+
+def rgb2gray(rgb):
+    return np.dot(rgb[...,:3], [0.299, 0.587, 0.114])
+def show_image(a,save=False,save_fname=None):
+    ''' display the image using matplotlib'''
+    plt.figure(figsize=(8,6))
+    a = np.uint8(np.clip(a, 0, 255))
+    plt.imshow(a)
+    plt.show()
+    if save:
+        if save_fname is None:
+            raise ValueError('save_fname must be set if save=True')
+        plt.imsave(save_fname,a)
+
+scale = 2
+
+img = cv2.imread('img.png', 1)
+img = preprocess(img, scale)
+print(img)
+cv2.imshow('test', img)
+cv2.waitKey()
